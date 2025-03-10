2091d69fd617fc203f874d23630304daf11412ff
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2025-03-10 22:41:19 +0000

    Use Dependency Injection

--- Cinnabar/Program.cs
+++ Cinnabar/Program.cs
@@ -1,22 +1,40 @@
 ﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
 using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 
 namespace Cinnabar;
 public class Program
 {
-    private static DiscordSocketClient _client;
+    private static IServiceProvider _serviceProvider;
+
+    static IServiceProvider CreateServices()
+    {
+        
+        var collection = new ServiceCollection()
+            .AddSingleton(new DiscordSocketConfig())
+            .AddSingleton(new DiscordSocketClient());
+
+        return collection.BuildServiceProvider();
+    }
+    
     public static async Task Main()
     {
-        _client = new DiscordSocketClient();
-        
-        _client.Log += Log;
+        var _serviceProvider = CreateServices();
+        var client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+        client.Log += Log;
 
+        var _interaction = new InteractionService(client.Rest);
+        
         var token = JsonConvert.DeserializeObject<Config>(File.ReadAllText("appsettings.json")).Token;
         
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+        
+        
         
         await Task.Delay(-1);
     }
