bba59661fd2a11b967d8e75e4f4266022b7c4b65
Joao Almeida <176084410+04ramalmeida@users.noreply.github.com>
2025-03-19 01:19:22 +0000

    Add a basic command (echo)

---
+++ Cinnabar/Modules/TestModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Interactions;
+
+namespace Cinnabar.Modules;
+
+public class TestModule : InteractionModuleBase
+{
+    [SlashCommand("echo", "Echo an imput")]
+    public async Task Echo(string input)
+    {
+        await RespondAsync(input);
+    }
+    
+    
+}
--- Cinnabar/Program.cs
+++ Cinnabar/Program.cs
@@ -1,4 +1,7 @@
-﻿using Discord;
+﻿using System.Net.Sockets;
+using System.Reflection;
+using Cinnabar.Modules;
+using Discord;
 using Discord.Commands;
 using Discord.Interactions;
 using Discord.WebSocket;
@@ -10,33 +13,63 @@ namespace Cinnabar;
 public class Program
 {
     private static IServiceProvider _serviceProvider;
+    private static DiscordSocketClient _client;
+    private static InteractionService _interaction;
 
     static IServiceProvider CreateServices()
     {
         
         var collection = new ServiceCollection()
             .AddSingleton(new DiscordSocketConfig())
-            .AddSingleton(new DiscordSocketClient());
+            .AddSingleton(new DiscordSocketClient())
+            .AddTransient<TestModule>();
 
         return collection.BuildServiceProvider();
     }
     
     public static async Task Main()
     {
-        var _serviceProvider = CreateServices();
-        var client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
-        client.Log += Log;
+        _serviceProvider = CreateServices();
+        _client = _serviceProvider.GetRequiredService<DiscordSocketClient>();
+        _client.Log += Log;
 
-        var _interaction = new InteractionService(client.Rest);
+        var interactionServiceConfig = new InteractionServiceConfig
+        {
+            LogLevel = LogSeverity.Debug,
+        };
+        _interaction = new InteractionService(_client, interactionServiceConfig);
+        _interaction.Log += Log;
         
         var token = JsonConvert.DeserializeObject<Config>(File.ReadAllText("appsettings.json")).Token;
         
-        await client.LoginAsync(TokenType.Bot, token);
-        await client.StartAsync();
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
         
-        
+
+        await _interaction.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
+        _client.Ready += async () =>  await _interaction.RegisterCommandsGloballyAsync();
+        _client.InteractionCreated += (socketInteraction) => InteractionCreated(socketInteraction);
+
         
         await Task.Delay(-1);
+    }
+
+    private static async Task InteractionCreated(SocketInteraction socketInteraction)
+    {
+        try
+        {
+            SocketInteractionContext ctx = new(_client, socketInteraction);
+
+            await _interaction.ExecuteCommandAsync(ctx, _serviceProvider);
+        }
+        catch
+        {
+            if (socketInteraction.Type == InteractionType.ApplicationCommand)
+            {
+                await socketInteraction.GetOriginalResponseAsync()
+                    .ContinueWith(async (msg) => msg.Result.DeleteAsync());
+            }
+        }
     }
 
     private static Task Log(LogMessage msg)
